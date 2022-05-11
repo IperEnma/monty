014579a258cccbbe00a4175afdf4727fe813672e
IperENma <enmanuelhernandez1843@gmail.com>
2022-05-11 13:22:17 -0700

    monty

--- monty.c
+++ monty.c
@@ -76,15 +76,13 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		write(2, "USAGE: monty file\n", 18);
+		fprintf(stderr, "%s\n", "USAGE: monty file");
 		exit(EXIT_FAILURE);
 	}
 	fp = fopen(argv[1], "rw+");
 	if (fp == NULL)
 	{
-		write(2, "Error: Can't open file ", 23);
-		write(2, argv[1], strlen(argv[1]));
-		write(2, "\n", 1);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (getline(&buffer, &size, fp) != -1)
