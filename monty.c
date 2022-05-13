b3e6123d4a1e60fa88ebe4688b60a541c6298a8f
Alex <asenges400@gmail.com>
2022-05-12 21:21:18 -0300

    fix

--- monty.c
+++ monty.c
@@ -49,10 +49,10 @@ int tokenizer(stack_t **head, char *buffer)
 	cmd = strtok(buffer, " \n\f\v\t\r");
 	integer = strtok(NULL, " \n\f\v\t\r");
 
-	if (cmd[0] == 35)
-		return (0);
 	if (cmd == NULL)
 		return (0);
+	if (cmd[0] == 35)
+		return (0);      
 	function.f = get_op_func(cmd);
 	if (function.f == NULL)
 	{
